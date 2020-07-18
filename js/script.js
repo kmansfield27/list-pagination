2cbbada17a82d5ab094c0e31d5ca9eb85d6f5ea6
kmansfield27 <36116244+kmansfield27@users.noreply.github.com>
2020-07-18 01:47:17 -0400

    Add search functionality and no results notification

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
 
-
-   // Set global variables
+   // Set global variables for the student list and # of students to display per page 
    const listItems = document.querySelectorAll('.student-item');
    const itemsPerPage = 10;
 
+   // Global variable for layout selection
+   const page = document.querySelector('.page');
+
+   // Create no results notification
+   const noResults = document.createElement('p');
+   page.appendChild(noResults);
+   noResults.textContent = 'There are no students that match your search.';
+   noResults.style.marginTop = '60px';
+   noResults.style.textAlign = 'center';
+   noResults.style.display = 'none';
 
    /*** 
       Function to divide the list into pages. The function accepts a list 
@@ -20,10 +23,9 @@ document.addEventListener('DOMContentLoaded', () => {
       which indices are displayed on the screen. As the page number is
       changed, the displayed indices will adjust. 
    ***/
-
-   const showPage = (list, page) => {
+   function showPage(list, page) {
       const startIndex = (page * itemsPerPage) - itemsPerPage; 
-      const endIndex = page * itemsPerPage - 1;
+      const endIndex = (page * itemsPerPage) - 1;
       for (let i = 0; i < list.length; i++) {
          if (i >= startIndex && i <= endIndex) {
             list[i].style.display = '';
@@ -33,104 +35,117 @@ document.addEventListener('DOMContentLoaded', () => {
       }
    }
 
-   /*** 
-      Function to create the pagination buttons at the bottom of the screen.
+   // Create pagination links at the bottom of the screen.
+   function appendPageLinks(list) {
 
-   ***/
-
-   const appendPageLinks = (list) => {
-      const div = document.createElement('div');
-      div.className = 'pagination';
-
-      const ul = document.createElement('ul');
+      // Binding to determine the # of page links needed in the pagination
       const pageCount = Math.ceil(list.length / itemsPerPage);
-      
-      document.querySelector('.page').appendChild(div);
+
+      // Create and append elements for pagination
+      const div = document.createElement('div');
+      const ul = document.createElement('ul');
+      page.appendChild(div);
+      div.className = 'pagination';
       div.appendChild(ul);
 
+      // Create the inner markup for each link in pagination
       for (let i = 1; i <= pageCount; i++) {
-         let li = document.createElement('li');
-         li.innerHTML = `<a href="#">${[i]}</a>`;
+         const li = document.createElement('li');
+         const a = document.createElement('a');
+         a.href = '#';
+         a.textContent = `${[i]}`;
          ul.appendChild(li);
+         li.appendChild(a);
+
+         // Add active class to first link by default
+         if (i === 1) {
+            a.className = 'active';
+         }
+         
+         // Callback to update the pagination links on click 
+         a.addEventListener('click', function (e) {
+            e.preventDefault();
+            const links = document.querySelectorAll('.pagination a'); 
+            const clicked = e.target;
+            const pageNumber = clicked.textContent;
+
+               // Remove and set active class to links
+               for (link of links) {
+                  link.className = '';
+               }
+               clicked.className = 'active';
+
+               // Adjust view to display appropriate list items
+               showPage(listItems, pageNumber); 
+            
+         });
+      }
+   }
+
+   // Search function
+   function searchList(searchInput) {
+
+      // Define a new array to pass results on search into
+      let searchResults = [];
+      const pagination = document.querySelector('.pagination');
+
+      for (let i = 0; i < listItems.length; i++) {
+         
+         // If search matches list item, push to new array and make visible          
+         if ( searchInput.value !== 0 && listItems[i].textContent.toLowerCase().includes(searchInput.value.toLowerCase() ) ) {
+            searchResults.push(listItems[i]);
+            listItems[i].style.display = '';
+         } else {    
+            listItems[i].style.display = 'none';
+         }
       }
 
-      const links = document.querySelectorAll('.pagination a');
+      // Remove initial pagination
+      pagination.parentNode.removeChild(pagination);
 
-      ul.firstElementChild.firstElementChild.className = 'active';
+      // Rerun showPage and appendPageLinks to refresh view
+      showPage(searchResults, 1);
+      appendPageLinks(searchResults);
 
-      ul.addEventListener('click', (e) => {
 
-         if (e.target.tagName === 'A') {
-            //e.preventDefault();
-            const clicked = e.target;
-            for (link of links) {
-               link.className = '';
-            }
-            clicked.className = 'active';
-
-            const page = clicked.textContent;
-            showPage(listItems, page); 
-         }
-      });
+      // Display no results message when no matches found
+      if (searchResults.length === 0) {
+         noResults.style.display = 'block';
+      } else {
+         noResults.style.display = 'none';
+      }
    }
 
 
-   /*
-      <div class="page-header cf">
-        <h2>Students</h2>
-        
-        <!-- student search HTML to add dynamically -->
-        <div class="student-search">
-          <input placeholder="Search for students...">
-          <button>Search</button>
-        </div>
-        <!-- end search -->
+   // Create search bar and pass in the search handler
+   function appendSearch() {
 
-      </div>
-   */
-
-   const appendSearch = () => {
+      // Create and append search elements
       const header = document.querySelector('.page-header');
       const div = document.createElement('div');
       div.className = 'student-search';
-      
       const input = document.createElement('input');
       input.placeholder = 'Search for students...';
-      
       const button = document.createElement('button');
       button.textContent = 'Search';
-
       header.appendChild(div);
       div.appendChild(input);
       div.appendChild(button);
+
+      // Click handler for search button
+      button.addEventListener( 'click', function (e) {
+         searchList(input);
+      });
+   
+      // Keyup handler for input
+      input.addEventListener( 'keyup', function (e) {
+         searchList(input);
+      });
    }
    
-
+   // Initialize functions
    showPage(listItems, 1); 
    appendPageLinks(listItems);
    appendSearch();
-
-   const input = document.querySelector('input');
-   const button = document.querySelector('button');
-
-   const searchList = (searchElement, list) => {
-      for (let i = 0; i <= list.length; i++) {
-         if ( searchElement.value.length !== 0 && 
-              list[i].textContent.toLowerCase().includes(searchElement.value.toLowerCase() ) ) {
-            
-            list[i].style.display = 'list-item';
-         } else {    
-            list[i].style.display = 'none';
-         }
-      }
-   }
-
-   button.addEventListener( 'click', (e) => {
-      searchList(input, listItems);
-   });
-
-   input.addEventListener( 'keyup', (e) => {
-      searchList(input, listItems);
-   });
 
 });
